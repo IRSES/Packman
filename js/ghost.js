29bd5be2b1911c6b4816c014ee607c4e61e926c7
IRSES <barmen3456besdna@gmail.com>
2023-09-21 18:42:32 +0300

    [AS-3] added lives with win-game over

--- js/ghost.js
+++ js/ghost.js
@@ -265,7 +265,7 @@ class Ghost {
         );
         canvasContext.restore();
         canvasContext.beginPath();
-        canvasContext.strokeStyle = "red";
+        canvasContext.strokeStyle = "rgba(117, 190, 218, 0)";
         canvasContext.arc(
             this.x + oneBlockSize / 2,
             this.y + oneBlockSize / 2,
